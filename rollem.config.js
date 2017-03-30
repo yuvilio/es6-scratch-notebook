4f80d15f17ea3170b892f74a2f52d7a3fe4ee2b3
Yuval Maman <yuviliom@gmail.com>
2017-03-29 21:43:06 -0400

    simpler config with Object.assign

--- rollem.config.js
+++ rollem.config.js
@@ -2,51 +2,43 @@
 import babel from 'rollup-plugin-babel' // "using Babel to transpile your ES6/7 code and Rollup to generate a standalone bundle", "Babel will respect .babelrc files"
 import resolve from 'rollup-plugin-node-resolve' // "Locate modules using the Node resolution algorithm, for using third party modules in node_modules"
 import commonjs from 'rollup-plugin-commonjs'
+
+var baseProps = {
+  format: 'iife',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**' // "Ideally, you should only be transforming your own source code", not external dependencies
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      sourceMap: true
+    }),
+    resolve({
+      jsnext: true,
+      main: true
+    }),
+
+    // uglify()
+  ],
+};
+
 export default [
   //one object per bundle
-  {
-    entry: './assets/js/functionaljs/01/001-example.js',
-    dest: './dist/assets/js/functionaljs/01/001-example.js',
-    format: 'iife',
-    plugins: [
-      babel({
-        exclude: 'node_modules/**' // "Ideally, you should only be transforming your own source code", not external dependencies
-      }),
-      commonjs({
-        include: 'node_modules/**',
-        sourceMap: true
-      }),
-      resolve({
-        jsnext: true,
-        main: true
-      }),
-
-      // uglify()
-    ],
-    moduleName: '001-example-js'
-  },
-
-  {
-    entry: './assets/js/functionaljs/01/002-example.js',
-    dest: './dist/assets/js/functionaljs/01/002-example.js',
-    format: 'iife',
-    plugins: [
-      babel({
-        exclude: 'node_modules/**' // "Ideally, you should only be transforming your own source code", not external dependencies
-      }),
-      commonjs({
-        include: 'node_modules/**',
-        sourceMap: true
-      }),
-      resolve({
-        jsnext: true,
-        main: true
-      }),
-
-      // uglify()
-    ],
-    moduleName: '002-example-js'
-  },
-
+  Object.assign(
+    baseProps,
+    {
+      entry: './assets/js/functionaljs/01/001-example.js',
+      dest: './dist/assets/js/functionaljs/01/001-example.js',
+      moduleName: '001-example-js'
+    }
+  ),
+  Object.assign(
+    baseProps,
+    {
+      entry: './assets/js/functionaljs/01/002-example.js',
+      dest: './dist/assets/js/functionaljs/01/002-example.js',
+      moduleName: '002-example-js'
+    }
+  )
 
 ]
