be45f90d7f2e09bcde52bca01d3bd6c37aca1781
Yuval Maman <yuviliom@gmail.com>
2017-03-30 02:32:44 -0400

    some arrow function notes

--- assets/js/basic-es6/01/002-arrow.js
+++ assets/js/basic-es6/01/002-arrow.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   console.log('here is 002-arrow ')
+
+  let getPrice = () => 2.45 // shorthand for anonmyous function. no 'function' wrapping or 'returns'
+    // says this function returns 2.45
+  console.log(typeof getPrice) // its a 'function' type
+  console.log('getPrice(): ', getPrice()) // 2.45
+
+  let getPriceArgs = (count, added) => count * 3.00 + added // passing arguments to the function
+  console.log('getPriceArgs(2, 4): ', getPriceArgs(2, 4)) // 10
+
+  let getPriceBlock = (count, taxed) => { // add {} when needing more than one line of code
+    let price = count * 2
+    if (taxed) {
+      price *= 1.055
+    }
+    return price // you'll need the return if using a block for the function
+  }
+  console.log('getPriceBlock(5, true): ', getPriceBlock(5, true)) // 10.55
+
+  // not set to element receiving the event. it's set to the context of the code
+  // document.addEventListener('click', () => { console.log(this) } )  //global context . Undefind or maybe Window object
+  document.querySelector('.btn-arrow').addEventListener('click', () => console.log(this))  // global context . Undefind or maybe Window object
+
+  let invoice = {
+    number: 323,
+    process: function () {
+      console.log('invoice this: ', this)
+    }
+  }
+  invoice.process() // you get the object literal(invoice)
+
+  let invoiceProcessArrow = {
+    number: 323,
+    process: () => console.log('invoiceProcessArrow.process(): ', this)
+  }
+  invoiceProcessArrow.process() // context o the code (document object or Window)
+
+  let getPrice2 = () => 2.45
+  console.log('getPrice2.hasOwnProperty(\'prototype\')', getPrice2.hasOwnProperty('prototype')) // chrome might still show true but that may be in flux. try it in a node console and you'll get false
 })
--- content/posts/basic-es6/01/002-arrrow.md
+++ content/posts/basic-es6/01/002-arrrow.md
@@ -6,9 +6,63 @@ excerpt: A
 ---
 
 
-Arrow function
+[Arrow function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
 ==============
 
+shorthand for a function
 <pre><code class="language-js">
+let getPrice = () => 2.45 // shorthand for anonmyous function. no 'function' wrapping or 'returns'
+  // says this function returns 2.45
+console.log( typeof getPrice) // its a 'function' type
+console.log( 'getPrice(): ', getPrice() ) //2.45
+
 
 </code></pre>
+
+* what if we want to pass arguments to the function?
+
+<pre><code class="language-js">
+let getPriceArgs = (count, added) => count * 3.00 + added // passing arguments to the function
+console.log('getPriceArgs(2, 4): ', getPriceArgs(2, 4)) // 10
+</code></pre>
+
+no parentheses needed if it's just one parameter
+
+
+* what if i need more than one line for my code?
+
+<pre><code class="language-js">
+let getPriceBlock = (count, taxed) => { //add {} when needing more than one line of code
+  let price = count * 2
+  if (taxed){
+    price *= 1.055
+  }
+  return price //you'll need the return if using a block for the function
+}
+console.log('getPriceBlock(5, true): ', getPriceBlock(5, true)) //10.55
+</code></pre>
+
+
+* this index
+
+<a href="#" class="btn btn-arrow">Click here</a>
+
+
+<pre><code class="language-js">
+let invoice = {
+  number: 123,
+  process: function () {
+    console.log('invoice this: ', this)
+  }
+}
+invoice.process() //you get the object literal(invoice)
+
+let invoiceProcessArrow = {
+  number: 123,
+  process: () => console.log('invoiceProcessArrow.process(): ', this)
+}
+invoiceProcessArrow.process() //context o the code (document object or Window)
+
+</code></pre>
+
+* no more re-assigning the 'this' context via bind/call/apply
