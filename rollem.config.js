b9baff787e95922a014f86972f9b899437c524d6
Yuval Maman <yuviliom@gmail.com>
2017-04-02 22:59:10 -0400

    split the bundles to separate modules so can limit builds happening at any one time

--- rollem.config.js
+++ rollem.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel' // "using Babel to transpile your ES6/7 
 import resolve from 'rollup-plugin-node-resolve' // "Locate modules using the Node resolution algorithm, for using third party modules in node_modules"
 import commonjs from 'rollup-plugin-commonjs'
 
-// TODO: break up the config to different rollem modules, so not generating a lot of bundles on each refresh 
+// TODO: break up the config to different rollem modules, so not generating a lot of bundles on each refresh
 
 var baseProps = {
   format: 'iife',
@@ -24,40 +24,8 @@ var baseProps = {
   ],
 };
 
-export default [
-  //one object per bundle
-  Object.assign(
-    baseProps,
-    {
-      entry: './assets/js/functionaljs/01/001-example.js',
-      dest: './dist/assets/js/functionaljs/01/001-example.js',
-      moduleName: '001-example-js'
-    }
-  ),
-  Object.assign(
-    baseProps,
-    {
-      entry: './assets/js/functionaljs/01/002-example.js',
-      dest: './dist/assets/js/functionaljs/01/002-example.js',
-      moduleName: '002-example-js'
-    }
-  ),
-  Object.assign(
-    baseProps,
-    {
-      entry: './assets/js/basic-es6/01/001-init.js',
-      dest: './dist/assets/js/basic-es6/01/001-init.js',
-      moduleName: 'basic-es6-001-init'
-    }
-  ),
-  Object.assign(
-    baseProps,
-    {
-      entry: './assets/js/basic-es6/01/002-arrow.js',
-      dest: './dist/assets/js/basic-es6/01/002-arrow.js',
-      moduleName: 'basic-es6-002-arrow'
-    }
-  ),
+//which bundles currently building
+import { bundles } from './lib/rollem/basic-es6/basic-es6-001.js';
+bundles.forEach( bundle => Object.assign(baseProps, bundle)  )
 
-
-]
+export default bundles
