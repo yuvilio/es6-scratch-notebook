42e72ae9eaa024f177e48d3758072c8de58b74bc
Yuval Maman <yuviliom@gmail.com>
2017-04-29 17:23:40 -0400

    another autoload example with interfaces to boot

---
+++ cli/phpmod/01/005-interface.php
@@ -0,0 +1,18 @@
+<?php
+
+// 005-interface.php
+
+require_once 'vendor/autoload.php';
+
+
+use Ch01\Ex003Pen\DaventryVII as DaventryVII; // use the DaventryVII class
+$daventry_8_prison = new DaventryVII(["Dwarf Thief"]);
+
+echo "Daventry VII prison, headed by " . $daventry_8_prison->getOfficer() . "\n";
+echo "prisoners: " . implode($daventry_8_prison->getPrisoners(), ", ") . "\n";
+
+// use Ex002Federation\DeepShipCruiserCells as DeepShipCruiserCells;
+//
+// $deep_ship_prison = new DeepShipCruiserCells();
+//
+// echo "Deep ship prison run by "
