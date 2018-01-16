52b5c3491a1b5d330756738494fa99fef92767dc
Yuval Maman <yuviliom@gmail.com>
2018-01-16 01:42:55 -0500

    remove events example from tutorial as it was a bit too large and clunky

--- assets/js/react-comp/03/002-events.js
+++ assets/js/react-comp/03/002-events.js
@@ -1,25 +1,7 @@
 // Import React and React-dom.
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { CommanderGreetFunc } from './components/004-forms-and-events.jsx'
-import { FormGreet } from './components/005-form-submit.jsx'
-import { Game } from './components/006-tic-tac-toe.jsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
 document.addEventListener('DOMContentLoaded', function (event) {
   console.log('here is 002-events')
-
-  ReactDOM.render(
-    <CommanderGreetFunc />,
-    document.querySelector('#ex001Form')
-  )
-
-  ReactDOM.render(
-    <FormGreet />,
-    document.querySelector('#ex001Form')
-  )
-
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('tictactoe')
-  )
 })
--- assets/js/react-comp/03/components/004-forms-and-events.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-let CommanderGreetFunc = (props) => {
-  return (
-    <div>Test div</div>
-  )
-}
-
-export { CommanderGreetFunc }
